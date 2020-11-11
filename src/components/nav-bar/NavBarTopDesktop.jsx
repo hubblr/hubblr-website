19180694ceec675e6e361f17561d68989ca5a2f6
Lennart Weihs <lennart@hubblr.io>
2020-11-11 13:30:00 +0100

    Fix background of desktop nav bar

--- src/components/nav-bar/NavBarTopDesktop.jsx
+++ src/components/nav-bar/NavBarTopDesktop.jsx
@@ -10,11 +10,12 @@ function NavBarTopDesktop({ contentWidth }) {
       className="h-20 w-full p-5 flex flex-row container mx-auto"
       style={{ width: contentWidth }}
     >
+      <div className="absolute inset-0 navbar-background-blur" />
       <div className="w-1/3" />
       <div className=" w-1/3 relative flex justify-center h-full">
         <NavigationButtonHubblrIcon />
       </div>
-      <div className="flex flex-col justify-center items-end w-1/3">
+      <div className="relative flex flex-col justify-center items-end w-1/3">
         <Link
           to="/contact"
           className="text-center font-extrabold leading-none tracking-tight p-4 hover:bg-brand-gray-darkest rounded-lg animate"
