aa16e972e816b2ea4325d6e6efb4400dc578f288
Lennart Weihs <lennart@hubblr.io>
2020-11-12 16:26:57 +0100

    fixed build process

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 import Seo from '../components/seo/Seo';
+import Layout from '../components/layouts/Layout';
 
 function NotFoundPage() {
   return (
-    <div>
+    <Layout>
       <Seo title="404: Not found" />
       <div>
         <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
           Looks like this page is a ghost that got abducted by aliens...
         </h2>
       </div>
-    </div>
+    </Layout>
   );
 }
 
