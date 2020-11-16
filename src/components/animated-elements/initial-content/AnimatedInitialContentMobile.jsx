3dcbd9ad8f15b2ff43cca22f91eddb9f11edc10f
Lennart Weihs <lennart@hubblr.io>
2020-11-16 19:02:31 +0100

    cleaned up sizing/scaling procedure

--- src/components/animated-elements/initial-content/AnimatedInitialContentMobile.jsx
+++ src/components/animated-elements/initial-content/AnimatedInitialContentMobile.jsx
@@ -30,7 +30,7 @@ const transforms = {
     },
     scale: {
       inputPercentages: [0, 50],
-      // output range is calculated by component
+      outputRange: [1.2, 1],
     },
   },
 };
@@ -57,26 +57,18 @@ function useLimitedDimensions(widthLimit, heightLimit, calcWidth, calcHeight) {
   return [maxWidth, heightLimit];
 }
 
-function AnimatedInitialContentMobile({ className, fadeInImage, contentTitle }) {
-  // get required values from context
-  const { animationAreaStartY, animationAreaStep, contentContainerRef } = useContext(
-    AnimationAreaContext
-  );
-
-  // dimensions that limit space for the initial content
-  const availableHeight = useAvailableHeight(contentContainerRef);
-  const imageContainerRef = useRef();
-  const imageContainerWidth = useClientWidth(imageContainerRef);
-  const imageContainerHeight = useClientHeight(imageContainerRef);
-
-  // find out the proportions of the image
-  const imageWrapperRef = useRef();
+function useImageScaling(imageWrapperRef, imageContainerWidth, availableHeight) {
   const [canCreateCallback, setCanCreateCallback] = useState(false);
+  // do NOT remove imageWrapperRef.current from deps!
   useEffect(() => {
     if (imageWrapperRef.current) {
       setCanCreateCallback(true);
     }
   }, [imageWrapperRef, imageWrapperRef.current]);
+
+  /* canCreateCallback flips a single time when imageWrapperRef.current is initially set. Thus, both
+  useMemo calls execute exactly once when no styles are set on the imageWrapper. The dimensions of
+  images should never change, so this is indeed the intended behavior. */
   const calcImageHeight = useMemo(() => {
     if (!canCreateCallback) {
       return () => {
@@ -88,7 +80,7 @@ function AnimatedInitialContentMobile({ className, fadeInImage, contentTitle }) 
     return (newWidth) => {
       return (height / width) * newWidth;
     };
-  }, [canCreateCallback]);
+  }, [canCreateCallback, imageWrapperRef]);
   const calcImageWidth = useMemo(() => {
     if (!canCreateCallback) {
       return () => {
@@ -100,7 +92,7 @@ function AnimatedInitialContentMobile({ className, fadeInImage, contentTitle }) 
     return (newHeight) => {
       return (width / height) * newHeight;
     };
-  }, [canCreateCallback]);
+  }, [canCreateCallback, imageWrapperRef]);
 
   // find initial height and width
   const initalWidthLimit = MOBILE_INITIAL_CONTENT.START.MAX_WIDTH * imageContainerWidth;
@@ -122,8 +114,8 @@ function AnimatedInitialContentMobile({ className, fadeInImage, contentTitle }) 
     calcImageHeight
   );
 
-  // set width and height of image wrapper
-  const imageWrapperStyles =
+  // create style object to set on image wrapper
+  const imageWrapperStyle =
     initialWidth === 0 && initialHeight === 0
       ? {}
       : {
@@ -133,16 +125,39 @@ function AnimatedInitialContentMobile({ className, fadeInImage, contentTitle }) 
   // derive necessary scale to arrive at final dimensions
   const scale = initialWidth ? finalWidth / initialWidth : 1;
 
-  const sectionHeadingRef = useRef();
-  // margin to move section title halfway into image
-  const sectionHeadingMarginTop = imageContainerHeight / 2;
+  return {
+    imageWrapperStyle,
+    scale,
+  };
+}
+
+function AnimatedInitialContentMobile({ className, fadeInImage, contentTitle }) {
+  // get required values from context
+  const { animationAreaStartY, animationAreaStep, contentContainerRef } = useContext(
+    AnimationAreaContext
+  );
+
+  // dimensions that limit space for the initial content
+  const availableHeight = useAvailableHeight(contentContainerRef);
+  const imageContainerRef = useRef();
+  const imageContainerWidth = useClientWidth(imageContainerRef);
+  const imageContainerHeight = useClientHeight(imageContainerRef);
+
+  // find out the proportions of the image
+  const imageWrapperRef = useRef();
+  const { imageWrapperStyle, scale } = useImageScaling(
+    imageWrapperRef,
+    imageContainerWidth,
+    availableHeight
+  );
   // offset towards image in initial state
   const contentYOffset = useInitialYOffset(availableHeight, MOBILE_INITIAL_CONTENT.Y);
+  // margin to move section title halfway into image
+  const sectionHeadingMarginTop = imageContainerHeight / 2;
 
   // finish transform descriptions
   transforms.initialContent.y.outputRange = [`${contentYOffset}px`, '0px'];
   transforms.fadeInImage.scale.outputRange = [scale, 1];
-  transforms.sectionHeading.scale.outputRange = [1.2, 1];
   // y offset of section title is derived from image scale values
   const fadeInScales = transforms.fadeInImage.scale.outputRange;
   const originalHeight = imageContainerHeight * fadeInScales[1];
@@ -182,16 +197,12 @@ function AnimatedInitialContentMobile({ className, fadeInImage, contentTitle }) 
     <div className={`relative flex flex-col ${className}`}>
       <div ref={imageContainerRef} className="w-full flex justify-center origin-center-top">
         <motion.div className="origin-center-top" style={fadeInImageStyles}>
-          <div ref={imageWrapperRef} style={imageWrapperStyles}>
+          <div ref={imageWrapperRef} style={imageWrapperStyle}>
             {fadeInImage}
           </div>
         </motion.div>
       </div>
-      <motion.div
-        ref={sectionHeadingRef}
-        className="relative z-10 origin-center-top"
-        style={sectionHeadingStyles}
-      >
+      <motion.div className="relative z-10 origin-center-top" style={sectionHeadingStyles}>
         <SectionHeading heading={contentTitle} />
       </motion.div>
     </div>
