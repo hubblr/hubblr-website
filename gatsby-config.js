6119bc4592bb3ebb5b8dc51176cada202b0762ff
Cedric Bals <cedric@appointer.com>
2020-11-17 15:41:13 +0100

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: fullConfig.theme.colors.white,
         theme_color: fullConfig.theme.colors.teal['400'],
         display: `minimal-ui`,
-        icon: `src/images/hubblr-icon.png`,
+        icon: `src/images/fav.png`,
       },
     },
     {
