6553029a942c99e1597c87ed6b98400fba324d6a
Lennart Weihs <lennart@hubblr.io>
2020-11-27 19:16:56 +0100

    critical fix to localizedNavigate

--- src/util/localizedNavigate.js
+++ src/util/localizedNavigate.js
@@ -1,8 +1,9 @@
 import { navigate } from '@reach/router';
+import { defaultLanguage } from '../globals';
 
 function localizedNavigate(to, locale, { replace = false } = {}) {
   // to can either point to new (index) path or subpath from current location
-  const localTo = to.substr(0, 1) === '/' ? `/${locale}${to}` : to;
+  const localTo = locale === defaultLanguage || to.substr(0, 1) !== '/' ? to : `/${locale}${to}`;
   // wrap navigate
   return new Promise((resolve) => {
     navigate(localTo, { replace }).then(() => {
