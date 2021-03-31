b62984f7473df59e40e0d18a7bb5551866f14ad8
Andreas Lempik <andreas.lempik@gmail.com>
2021-03-31 12:06:42 +0200

    minors

--- src/pages/consulting.jsx
+++ src/pages/consulting.jsx
@@ -1,20 +1,19 @@
+import { Link } from '@reach/router';
+import { FormattedMessage } from 'react-intl';
 import React from 'react';
 import { DesktopQuery, MobileAndTabletQuery } from '../util/helpers';
 import SectionHeading from '../components/index/section-heading/SectionHeading';
-import MainContentCard from '../components/index/main-content-card/MainContentCard';
-import MainContentGrid from '../components/index/main-content-card/MainContentGrid';
-import MainContentGridItem from '../components/index/main-content-card/MainContentGridItem';
-import HubblrPageLinks from '../components/links/HubblrPageLinks';
+// import MainContentCard from '../components/index/main-content-card/MainContentCard';
+// import MainContentGrid from '../components/index/main-content-card/MainContentGrid';
+// import MainContentGridItem from '../components/index/main-content-card/MainContentGridItem';
 import IndexGradientBorderButtonLongArrow from '../components/buttons/gradient-border-buttons/IndexGradientBorderButtonLongArrow';
 import ConsultingImage from '../images/consulting/consulting-illustration.png';
 import MainPageDarkLayout from '../components/layouts/MainPageDarkLayout';
 import NavBarMainPage from '../components/nav-bar/NavBarMainPage';
 import NavBarContactButton from '../components/nav-bar/NavBarContactButton';
+import ConsultingSectionContent from '../components/index/page-sections/ConsultingSectionContent';
 
 function ConsultingPage() {
-  // const contentRef = useRef();
-  // const contentWidth = useClientWidth(contentRef);
-
   return (
     <MainPageDarkLayout>
       <NavBarMainPage
@@ -31,71 +30,26 @@ function ConsultingPage() {
         <img className="w-20 h-auto" src={ConsultingImage} alt="satellite" />
 
         <MobileAndTabletQuery>
-          <MainContentCard
-            description="Wir beurteilen die technische Entwicklung bestehender Produkte"
-            title="Solution Assessment"
-            className="mb-5"
-          >
-            <MainContentGrid className="mb-20">
-              <MainContentGridItem
-                text="Welche Technologie-entscheidungen wurden getroffen & passen diese zum Status des Projektes? Welche Softwarequalität hat das aktuelle Projekt?"
-                title="Aufarbeitung Tech-Stack & Produkt"
-              />
-              <MainContentGridItem
-                text="Welche Hürden liegen im Entwicklungsprozess und welche Abhängigkeiten liegen an den Funktionären des Teams?"
-                title="Beurteilung Entwicklungsprozesse"
-              />
-              <MainContentGridItem
-                text="Wurde das Projekt angemessen entwickelt bzw. ist eine Konzipierung für eine angemessen skalierbare Anwendung erfolgt?"
-                title="Zukunftssicherheit feststellen"
-              />
-              <MainContentGridItem
-                text="Beratung zur Umsetzung neuer Entwicklungsprozesse oder Einführung modernerer Technologien."
-                title="Umsetzungs-Beratung"
-              />
-            </MainContentGrid>
-            <IndexGradientBorderButtonLongArrow
-              theme="light"
-              widthClass="w-full"
-              addedFlexClasses="justify-center"
-            >
-              Jetzt kontaktieren
-            </IndexGradientBorderButtonLongArrow>
-          </MainContentCard>
+          <ConsultingSectionContent>
+            <Link to="/contact">
+              <IndexGradientBorderButtonLongArrow
+                theme="light"
+                widthClass="w-full"
+                addedFlexClasses="justify-center"
+              >
+                <FormattedMessage id="generic.contact" />
+              </IndexGradientBorderButtonLongArrow>
+            </Link>
+          </ConsultingSectionContent>
         </MobileAndTabletQuery>
         <DesktopQuery>
-          <MainContentCard
-            description="Wir beurteilen die technische Entwicklung bestehender Produkte"
-            title="Solution Assessment"
-            className="mb-5"
-          >
-            <MainContentGrid>
-              <MainContentGridItem
-                text="Welche Technologie-entscheidungen wurden getroffen & passen diese zum Status des Projektes? Welche Softwarequalität hat das aktuelle Projekt?"
-                title="Aufarbeitung Tech-Stack & Produkt"
-              />
-              <MainContentGridItem
-                text="Welche Hürden liegen im Entwicklungsprozess und welche Abhängigkeiten liegen an den Funktionären des Teams?"
-                title="Beurteilung Entwicklungsprozesse"
-              />
-              <MainContentGridItem
-                text="Wurde das Projekt angemessen entwickelt bzw. ist eine Konzipierung für eine angemessen skalierbare Anwendung erfolgt?"
-                title="Zukunftssicherheit feststellen"
-              />
-              <MainContentGridItem
-                text="Beratung zur Umsetzung neuer Entwicklungsprozesse oder Einführung modernerer Technologien."
-                title="Umsetzungs-Beratung"
-              />
-            </MainContentGrid>
-          </MainContentCard>
-          <IndexGradientBorderButtonLongArrow theme="light">
-            Jetzt kontaktieren
-          </IndexGradientBorderButtonLongArrow>
+          <ConsultingSectionContent />
+          <Link to="/contact">
+            <IndexGradientBorderButtonLongArrow theme="light">
+              <FormattedMessage id="generic.contact" />
+            </IndexGradientBorderButtonLongArrow>
+          </Link>
         </DesktopQuery>
-
-        <div className>
-          <HubblrPageLinks />
-        </div>
       </div>
     </MainPageDarkLayout>
   );
--- src/pages/ventures.jsx
+++ src/pages/ventures.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from '@reach/router';
+import { FormattedMessage } from 'react-intl';
 import VenturesArrowImage from '../images/ventures/venturesArrow.png';
 import SectionHeading from '../components/index/section-heading/SectionHeading';
 import { DesktopQuery, MobileAndTabletQuery } from '../util/helpers';
-import MainContentCard from '../components/index/main-content-card/MainContentCard';
-import MainContentGrid from '../components/index/main-content-card/MainContentGrid';
-import MainContentGridItem from '../components/index/main-content-card/MainContentGridItem';
+// import MainContentCard from '../components/index/main-content-card/MainContentCard';
+// import MainContentGrid from '../components/index/main-content-card/MainContentGrid';
+// import MainContentGridItem from '../components/index/main-content-card/MainContentGridItem';
 import IndexGradientBorderButtonLongArrow from '../components/buttons/gradient-border-buttons/IndexGradientBorderButtonLongArrow';
 import MainPageDarkLayout from '../components/layouts/MainPageDarkLayout';
 import NavBarMainPage from '../components/nav-bar/NavBarMainPage';
 import NavBarContactButton from '../components/nav-bar/NavBarContactButton';
+import VenturesSectionContent from '../components/index/page-sections/VenturesSectionContent';
 
 function VenturesPage() {
   // TODO: move duplicated main content text to its own component
@@ -28,66 +31,25 @@ function VenturesPage() {
         <SectionHeading heading="Ventures" />
         <img className="w-40 h-auto" src={VenturesArrowImage} alt="ventures" />
         <MobileAndTabletQuery>
-          <MainContentCard
-            description="Wir glauben an Visionär*innen. Gemeinsam verändern wir die Welt."
-            title="Solution Assessment"
-            className="mb-5"
-          >
-            <MainContentGrid>
-              <MainContentGridItem
-                text="Gründe mit uns deine B2B SaaS-Solution aus. Wir helfen dir bei allen nötigen Schritten und bringen ein Work- oder Geldinvestment mit."
-                title="Investment & Gründung"
-              />
-              <MainContentGridItem
-                text="Wir entwickeln ein kosteneffizientes MVP und lernen gemeinsam frühzeitig von deinen Kunden."
-                title="User-Testing & Projektplanung"
-              />
-              <MainContentGridItem
-                text="Auch wenn dein Projekt komplex ist, legen wir jederzeit großen Wert auf unsere Zukunft und stellen sicher, dass das Projekt dem Ansturm gerecht wird."
-                title="Skalierbarkeit und Zukunftssicherheit"
-              />
-              <MainContentGridItem
-                text="Unser Team ist flexibel und bestehet zum großen Teil selber aus Unternehmer*innen. Mit diesem Spirit treibst auch du dein Projekt voran."
-                title="Starke Partner für die Zukunft"
-              />
-            </MainContentGrid>
-            <IndexGradientBorderButtonLongArrow
-              theme="light"
-              widthClass="w-full"
-              addedFlexClasses="justify-center"
-            >
-              Jetzt kontaktieren
-            </IndexGradientBorderButtonLongArrow>
-          </MainContentCard>
+          <VenturesSectionContent>
+            <Link to="/contact">
+              <IndexGradientBorderButtonLongArrow
+                theme="light"
+                widthClass="w-full"
+                addedFlexClasses="justify-center"
+              >
+                <FormattedMessage id="generic.contact" />
+              </IndexGradientBorderButtonLongArrow>
+            </Link>
+          </VenturesSectionContent>
         </MobileAndTabletQuery>
         <DesktopQuery>
-          <MainContentCard
-            description="Wir glauben an Visionär*innen. Gemeinsam verändern wir die Welt."
-            title="Solution Assessment"
-            className="mb-5"
-          >
-            <MainContentGrid>
-              <MainContentGridItem
-                text="Gründe mit uns deine B2B SaaS-Solution aus. Wir helfen dir bei allen nötigen Schritten und bringen ein Work- oder Geldinvestment mit."
-                title="Investment & Gründung"
-              />
-              <MainContentGridItem
-                text="Wir entwickeln ein kosteneffizientes MVP und lernen gemeinsam frühzeitig von deinen Kunden."
-                title="User-Testing & Projektplanung"
-              />
-              <MainContentGridItem
-                text="Auch wenn dein Projekt komplex ist, legen wir jederzeit großen Wert auf unsere Zukunft und stellen sicher, dass das Projekt dem Ansturm gerecht wird."
-                title="Skalierbarkeit und Zukunftssicherheit"
-              />
-              <MainContentGridItem
-                text="Unser Team ist flexibel und bestehet zum großen Teil selber aus Unternehmer*innen. Mit diesem Spirit treibst auch du dein Projekt voran."
-                title="Starke Partner für die Zukunft"
-              />
-            </MainContentGrid>
-          </MainContentCard>
-          <IndexGradientBorderButtonLongArrow theme="light">
-            Jetzt kontaktieren
-          </IndexGradientBorderButtonLongArrow>
+          <VenturesSectionContent />
+          <Link to="/contact">
+            <IndexGradientBorderButtonLongArrow theme="light">
+              <FormattedMessage id="generic.contact" />
+            </IndexGradientBorderButtonLongArrow>
+          </Link>
         </DesktopQuery>
       </div>
     </MainPageDarkLayout>
