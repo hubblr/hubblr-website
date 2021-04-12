49e96734c32a7f340e9ac7eb22dbdcb6d9dc1e87
Andreas Lempik <andreas.lempik@gmail.com>
2021-04-12 19:15:10 +0200

    added video :tada: and finally get overlay on video working :rotating_light:

--- src/components/index/page-sections/IntroductionSection.jsx
+++ src/components/index/page-sections/IntroductionSection.jsx
@@ -4,29 +4,43 @@ import PropTypes from 'prop-types';
 // import { DesktopQuery } from '../../../util/helpers';
 // import SectionScrollBar from '../section-scroll-bar/SectionScrollBar';
 // import ArrowImageDownDouble from '../../image-components/ArrowImageDownDouble';
+import WebsiteHeroVideo from '../../videos/web-hero.mp4';
 
 function IntroductionSection({ children }) {
   return (
-    <div className="container mx-auto min-h-screen flex flex-col h-screen mt-4 ">
-      <div className="flex-grow flex flex-col lg:justify-center pt-20 pb-5 lg:pt-0 md:pb-0">
-        {children}
+    <>
+      <video
+        muted
+        autoPlay
+        loop
+        className="absolute min-h-full min-w-full w-auto h-full max-w-none z-0"
+      >
+        <source src={WebsiteHeroVideo} type="video/mp4" />
+      </video>
+      <div className=" absolute flex-grow flex min-h-screen flex-col w-screen h-screen bg-black bg-opacity-75 z-30">
+        <p> </p>
       </div>
-      {/* <MobileAndTabletQuery>
+      <div className="container mx-auto min-h-screen flex flex-col h-screen mb-40 ">
+        <div className="flex-grow flex flex-col lg:justify-center pt-20 pb-5 lg:pt-0 md:pb-0 z-50">
+          {children}
+        </div>
+        {/* <MobileAndTabletQuery>
         <div className="w-full flex justify-center mb-5">
-          <ArrowImageDownDouble />
+        <ArrowImageDownDouble />
         </div>
       </MobileAndTabletQuery> */}
-      {/* <DesktopQuery>
+        {/* <DesktopQuery>
         <div className="mt-4">
-          <SectionScrollBar
-            upperFlexGrow={0}
-            lowerFlexGrow={0}
-            lowerClassNames="h-16"
-            arrowImage={<ArrowImageDownDouble />}
-          />
+        <SectionScrollBar
+        upperFlexGrow={0}
+        lowerFlexGrow={0}
+        lowerClassNames="h-16"
+        arrowImage={<ArrowImageDownDouble />}
+        />
         </div>
       </DesktopQuery> */}
-    </div>
+      </div>
+    </>
   );
 }
 
