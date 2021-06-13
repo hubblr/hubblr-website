169f23f5237bf51837a0e3c9fba77741d98e0133
Andreas Lempik <andreas.lempik@gmail.com>
2021-06-13 14:13:18 +0200

    changes to website hero: adding video poster; rearranging website hero layers

--- src/components/index/page-sections/IntroductionSection.jsx
+++ src/components/index/page-sections/IntroductionSection.jsx
@@ -8,17 +8,17 @@ function IntroductionSection({ children }) {
     <div className="relative overflow-hidden items-center justify-center h-screen mb-40">
       <DesktopQuery>
         {/* DARK OVERLAY FOR VIDEO */}
-        <div className="absolute top-0 left-0 h-full w-full bg-black bg-opacity-50 z-10">
-          <video
-            playsInline
-            muted
-            autoPlay
-            loop
-            className="absolute w-auto min-w-full min-h-full max-w-none background-video z-0"
-          >
-            <source src={WebsiteHeroVideo} type="video/mp4" />
-          </video>
-        </div>
+        <div className="absolute top-0 left-0 h-full w-full bg-black bg-opacity-50 z-10" />
+        <video
+          playsInline
+          muted
+          autoPlay
+          loop
+          posterImage="../../../images/hubblr-website-hero-video-poster-image.jpeg"
+          className="absolute w-auto min-w-full min-h-full max-w-none background-video z-0"
+        >
+          <source src={WebsiteHeroVideo} type="video/mp4" />
+        </video>
       </DesktopQuery>
       <div className="relative container mx-auto min-h-screen flex flex-col h-screen z-20">
         <div className="flex-grow flex flex-col lg:justify-center pt-20 pb-5 lg:pt-0 md:pb-0 z-0">
