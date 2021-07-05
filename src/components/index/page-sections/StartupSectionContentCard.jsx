bb75e340e2a4d852b7567b855fd8c2cca16587f2
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-07-05 13:25:08 +0200

    delete dependency

--- src/components/index/page-sections/StartupSectionContentCard.jsx
+++ src/components/index/page-sections/StartupSectionContentCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from '@reach/router';
 import { FormattedMessage } from 'react-intl';
 
 const StartupSectionContentCard = ({ firm, description, link, className }) => {
